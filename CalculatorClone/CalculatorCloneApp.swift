f30ac4d3e52eb67d25e998a74a71cdcad74f0754
Nam Geon Kim <namgeon1106@naver.com>
2022-05-28 01:42:31 +0900

    Initial Commit

---
+++ CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by 김남건 on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
