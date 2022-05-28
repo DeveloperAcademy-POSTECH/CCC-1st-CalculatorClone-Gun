e12996465fff006552753917aed8749f0678e0d3
Nam Geon Kim <namgeon1106@naver.com>
2022-05-28 13:19:52 +0900

    feat: ButtonContent enum 추가

---
+++ CalculatorClone/ButtonContent.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonInput.swift
+//  CalculatorClone
+//
+//  Created by 김남건 on 2022/05/28.
+//
+
+import Foundation
+import SwiftUI
+
+enum ButtonContent {
+    case reset, plusMinus, percent,
+         one, two, three, four, five, six, seven, eight, nine, zero, dot,
+         divide, multiply, minus, plus, equals
+}
