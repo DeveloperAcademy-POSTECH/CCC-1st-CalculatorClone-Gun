1c5c60bc512dd8f5b204b764afef83627d9374b5
Nam Geon Kim <namgeon1106@naver.com>
2022-05-28 13:20:39 +0900

    feat: ButtonContent에 fore/background color property 추가

--- CalculatorClone/ButtonContent.swift
+++ CalculatorClone/ButtonContent.swift
@@ -12,4 +12,24 @@ enum ButtonContent {
     case reset, plusMinus, percent,
          one, two, three, four, five, six, seven, eight, nine, zero, dot,
          divide, multiply, minus, plus, equals
+
+    var backgroundColor: Color {
+        switch self {
+        case .reset, .plusMinus, .percent:
+            return Color(UIColor.systemGray2)
+        case .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .dot:
+            return Color(UIColor.systemGray6)
+        case .divide, .multiply, .minus, .plus, .equals:
+            return .orange
+        }
+    }
+
+    var foregroundColor: Color {
+        switch self {
+        case .reset, .plusMinus, .percent:
+            return .black
+        default:
+            return .white
+        }
+    }
 }
