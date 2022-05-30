33c95f1aebdc7d17f436c75ef7565578b04fed9b
Nam Geon Kim <namgeon1106@naver.com>
2022-05-30 19:53:50 +0900

    feat: CalculatorScreen 전체적인 화면 구성 구현

--- CalculatorClone/CalculatorScreen.swift
+++ CalculatorClone/CalculatorScreen.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct CalculatorScreen: View {
+    @State private var currentInput = 0.0
+    let buttonContentRows: [[CalculatorButtonContent]] = [
+        [.reset, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equals]
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("\(Int(currentInput))")
+                .font(.system(size: 85, weight: .thin))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.horizontal, 15)
+                .padding(.bottom, 5)
+            ForEach(buttonContentRows, id: \.self) { buttonContentRow in
+                HStack {
+                    ForEach(buttonContentRow, id: \.self) { buttonContent in
+                        Button {
+
+                        } label: {
+                            buttonContent.label
+                        }
+                        .buttonStyle(CalculatorButtonStyle(buttonContent: buttonContent))
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 30)
+        .preferredColorScheme(.dark)
     }
 }
 
