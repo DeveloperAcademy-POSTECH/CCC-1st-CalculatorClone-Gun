c37bd9f56f6607a54df4e55fff32c8145d8ea083
Nam Geon Kim <namgeon1106@naver.com>
2022-05-30 12:57:30 +0900

    feat: button spacing 상수 추가

---
+++ CalculatorClone/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CalculatorClone
+//
+//  Created by 김남건 on 2022/05/30.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let buttonSpacing: CGFloat = 12
+}
