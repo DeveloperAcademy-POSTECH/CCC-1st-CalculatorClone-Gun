8ee49d30df396e8e57ad99a95539f114af8b153d
namgeon1106 <namgeon1106@naver.com>
2022-06-08 20:22:43 +0900

    feat: CalculationNode 추가

---
+++ CalculatorClone/Models/CalculationNode.swift
@@ -0,0 +1,13 @@
+//
+//  CalculationNode.swift
+//  CalculatorClone
+//
+//  Created by 김남건 on 2022/06/08.
+//
+
+import Foundation
+
+struct CalculationNode {
+    let calcOperator: CalculatorButtonContent
+    let calcOperand: Double
+}
